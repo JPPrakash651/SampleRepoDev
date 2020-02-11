eafff2d95bbba108be8c2dc6a05e3b8ea0a71226
JPPrakash651 <jpprakash651@gmail.com>
2020-02-12 01:57:29 +0530

    from home

--- src/test/java/Test/launchBrowser.java
+++ src/test/java/Test/launchBrowser.java
@@ -10,20 +10,26 @@ public class launchBrowser {
 	public static void main(String[] args) throws InterruptedException 
 	{
 		System.setProperty("webdriver.chrome.driver", 
-				"C:\\Users\\120190\\Documents\\Personal\\SW\\Sample Automation\\SampleRepoDev\\Driver\\chromedriver.exe");
+				"D:\\Selenium Project\\SampleProject\\Driver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com/");
-		WebElement txtSearch = driver.findElement(By.xpath("//input[@title='Search']"));
-		txtSearch.sendKeys("HDFC login");
+		WebElement txtSearch = driver.findElement(By.name("q"));
+		txtSearch.sendKeys("HDFC login netbanking");
 		WebElement btnSearch = driver.findElement(By.xpath("(//input[@value='Google Search'])[2]"));
 		btnSearch.click();
 		WebElement lnkSearchResult = driver.findElement(By.xpath("//h3[text()='NetBanking - HDFC Bank']"));
 		lnkSearchResult.click(); 
-		driver.switchTo().frame(0);
-		WebElement btnContinueHDFC = driver.findElement(By.xpath("//a[text()='Continue to NetBanking']"));
-		btnContinueHDFC.click();
-		
+		driver.switchTo().frame("login_page");
+		WebElement txtUserName = driver.findElement(By.name("fldLoginUserId"));
+		txtUserName.sendKeys("123456");
+		WebElement btnContinue = driver.findElement(By.xpath("//img[@alt='continue']"));
+		btnContinue.click(); Thread.sleep(2000);
+		WebElement txtPassword = driver.findElement(By.name("fldPassword"));
+		txtPassword.sendKeys("123456");
+		WebElement btnContinue1 = driver.findElement(By.xpath("//img[@alt='Login']"));
+		btnContinue1.click();
 	}
 
 }
