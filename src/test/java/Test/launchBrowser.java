f125cd50edece85e498dbe7d9f533223704ddf19
Ponnusamy <JayaprakashPonnusamy2@ToppanMerrillLLC.com>
2020-02-11 21:33:33 +0530

    From office

--- src/test/java/Test/launchBrowser.java
+++ src/test/java/Test/launchBrowser.java
@@ -7,20 +7,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class launchBrowser {
 	
-	public static void main(String[] args) 
+	public static void main(String[] args) throws InterruptedException 
 	{
 		System.setProperty("webdriver.chrome.driver", 
-				"D:\\Selenium Project\\SampleProject\\Driver\\chromedriver.exe");
+				"C:\\Users\\120190\\Documents\\Personal\\SW\\Sample Automation\\SampleRepoDev\\Driver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com/");
 		WebElement txtSearch = driver.findElement(By.xpath("//input[@title='Search']"));
 		txtSearch.sendKeys("HDFC login");
-		WebElement btnSearch = driver.findElement(By.xpath("(//input[@data-ved='0ahUKEwjzv6Lx1sfnAhWr8HMBHe7rDo4Q4dUDCAo']"));
+		WebElement btnSearch = driver.findElement(By.xpath("(//input[@value='Google Search'])[2]"));
 		btnSearch.click();
-		
 		WebElement lnkSearchResult = driver.findElement(By.xpath("//h3[text()='NetBanking - HDFC Bank']"));
-		lnkSearchResult.click();
+		lnkSearchResult.click(); 
+		driver.switchTo().frame(0);
+		WebElement btnContinueHDFC = driver.findElement(By.xpath("//a[text()='Continue to NetBanking']"));
+		btnContinueHDFC.click();
 		
 	}
 
