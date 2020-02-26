6efa79cc9e858d9e3ee08679b5286a31820277a0
JPPrakash651 <jpprakash651@gmail.com>
2020-02-27 00:56:19 +0530

    home_2

--- src/test/java/Sample1/StepDef.java
+++ src/test/java/Sample1/StepDef.java
@@ -1,13 +1,17 @@
 package Sample1;
 
+import org.openqa.selenium.WebDriver;
+
+import Sample.POM;
 import Sample2.LibGlobal;
-import net.bytebuddy.utility.privilege.GetSystemPropertyAction;
 
 public class StepDef extends LibGlobal {
+	static POM pom;
+	static WebDriver driver;
 	public static void main(String[] args) {
-//		launchBrowse();
+		driver = launchBrowse();
 		launchURL("https://www.google.com/");
-		txtBoxWrite(getSearchBox(), "HDFC login");
+		txtBoxWrite(pom.getSearchBox(), "HDFC login");
 	}
 
 }
--- src/test/java/Sample2/LibGlobal.java
+++ src/test/java/Sample2/LibGlobal.java
@@ -5,20 +5,18 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LibGlobal  {
-	static WebDriver driver;
+	public static WebDriver driver;
 
-//	public static void launchBrowse() {
-//		System.setProperty("webdriver.chrome.driver",
-//				"C:\\Users\\120190\\Documents\\Personal\\SW\\Sample Automation\\SampleRepoDev\\Driver\\chromedriver.exe");
-//		WebDriver driver = new ChromeDriver();
-//		driver.manage().window().maximize();
-//	}
+	public static WebDriver launchBrowse() {
+		System.setProperty("webdriver.chrome.driver",
+				"D:\\Selenium Project\\SampleProject\\Driver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		return driver;
+	}
 
 	public static void launchURL(String s) {
-		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\120190\\Documents\\Personal\\SW\\Sample Automation\\SampleRepoDev\\Driver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
+		
 		driver.get(s);
 	}
 	public static void txtBoxWrite(WebElement e, String a1) {
